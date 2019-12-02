256e730b72e0435bb77af6ac4c90158a70163304
noob-wale <wallacesheundah@gmail.com>
2019-12-02 13:10:05 +0300

    update end.js

--- end.js
+++ end.js
@@ -2,16 +2,30 @@ const userName = document.getElementById('username');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const saveBtn = document.getElementById('saveBtn');
+const MAX_HIGHSCORES = 5;
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []
 
 finalScore.innerText = mostRecentScore;
 
 userName.addEventListener('keyup', userValue);
 
 function userValue() {
-    console.log(userName.value);
     saveBtn.disabled = !userName.value;
-}
+};
 
 function saveHighScore(e) {
     e.preventDefault();
-}
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: userName.value
+    };
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
+    
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("./index.html");
+};
+
